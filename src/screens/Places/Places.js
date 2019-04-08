1b170a02e4a497cb375cfb0422cd1f977153b616
Patrick <Patrickmao@live.com>
2019-04-08 19:43:39 +0800

    working

--- src/screens/Places/Places.js
+++ src/screens/Places/Places.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Modal } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
 import * as actions from '../../store/actions';
